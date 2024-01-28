720ad0cec1be1e265c8dad6e827c1e9b2884840a
Bryan Robinson <bryanlrobinson@gmail.com>
2024-01-28 09:16:05 -0500

    Passes through redirects file

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ const sanityClient = require('./utilities/sanityClient')
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./images");
     eleventyConfig.addPassthroughCopy("./style.css");
+    eleventyConfig.addPassthroughCopy("./_redirects");
     eleventyConfig.addPlugin(sanityImage, {
         client: sanityClient // This is your Sanity connection object
       })
